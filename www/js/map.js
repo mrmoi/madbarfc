9c2b1bad622dc57c1458938ecf5ae4f3e3b3e3c2
mrmoi <mlmart16@asu.edu>
2015-12-17 01:10:58 -0700

    google maps 2nd attempt

--- www/js/map.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    scrollwheel: false,
-    zoom: 8
-
-  });
-}
