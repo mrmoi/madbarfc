e91158b95264146724ba90fc7f5e6435c8fba8af
Moises Martinez <mlmart16@asu.edu>
2015-12-17 14:21:23 -0700

    third map and styles

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -40,11 +40,11 @@ angular.module('starter.controllers', [])
     var mapOptions = {
             center: myLatLng,
             zoom: 17,
-            mapTypeId: google.maps.MapTypeId.SATELLITE
+            mapTypeId: google.maps.MapTypeId.HYBRID
     };
     var map = new google.maps.Map(document.getElementById("map"),
         mapOptions);
-        var contentString = "<div><a ng-click='clickTest()'>MadBar FC Traning Location</a></div>";
+        var contentString = "<div><a ng-click='clickTest()'>MadBar FC - Coronado Park <br>Traning Location</a></div>";
             var compiled = $compile(contentString)($scope);
             var infowindow = new google.maps.InfoWindow({
               content: compiled[0]
@@ -58,6 +58,54 @@ angular.module('starter.controllers', [])
               infowindow.open(map,marker);
             });
             $scope.map = map;
+
+            // second map
+            var myLatLng = new google.maps.LatLng(33.376751, -112.140417);
+            var mapOptions = {
+                    center: myLatLng,
+                    zoom: 17,
+                    mapTypeId: google.maps.MapTypeId.HYBRID
+            };
+            var map = new google.maps.Map(document.getElementById("map2"),
+                mapOptions);
+                var contentString = "<div><a ng-click='clickTest()'>MadBar FC - Cesar Chavez Park <br>Traning Location</a></div>";
+                    var compiled = $compile(contentString)($scope);
+                    var infowindow = new google.maps.InfoWindow({
+                      content: compiled[0]
+                    });
+                    var marker = new google.maps.Marker({
+                      position: myLatLng,
+                      map: map,
+                      title: 'Coronado Park, Phoenix AZ'
+                    });
+                    google.maps.event.addListener(marker, 'click', function() {
+                      infowindow.open(map,marker);
+                    });
+                    $scope.map = map;
+
+                // third map
+                    var myLatLng = new google.maps.LatLng(33.603620, -112.160552);
+                    var mapOptions = {
+                            center: myLatLng,
+                            zoom: 17,
+                            mapTypeId: google.maps.MapTypeId.HYBRID
+                    };
+                    var map = new google.maps.Map(document.getElementById("map3"),
+                        mapOptions);
+                        var contentString = "<div><a ng-click='clickTest()'>MadBar FC - ASU West<br>Traning Location</a></div>";
+                            var compiled = $compile(contentString)($scope);
+                            var infowindow = new google.maps.InfoWindow({
+                              content: compiled[0]
+                            });
+                            var marker = new google.maps.Marker({
+                              position: myLatLng,
+                              map: map,
+                              title: 'Coronado Park, Phoenix AZ'
+                            });
+                            google.maps.event.addListener(marker, 'click', function() {
+                              infowindow.open(map,marker);
+                            });
+                            $scope.map = map;
           }
           google.maps.event.addDomListener(window, 'load', initialize);
           })
