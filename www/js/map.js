c055709d2249df389ab38e962591284dfa989b3a
mrmoi <mlmart16@asu.edu>
2015-12-17 05:30:21 -0700

    maps setup

--- www/js/map.js
+++ www/js/map.js
@@ -1,7 +1,14 @@
 var map;
+var map2;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 33.467, lng: -112.056},
     zoom: 18
   });
+
+  map2 = new google.maps.Map(document.getElementById('map2'), {
+    center: {lat: 33.467, lng: -112.056},
+    zoom: 18
+  });
 }
+google.maps.event.addDomListener(window,'load', initMap);
