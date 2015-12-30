1983e275f6c9b940049a2650440e73a5073f24a0
mrmoi <mlmart16@asu.edu>
2015-12-30 01:26:20 -0700

    adding modal or popup to player list

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -53,6 +53,34 @@ angular.module('starter.controllers', [])
 .controller('StandingsController', function($scope, $http) {
     $http.get('standings.json').success(function(data) {
       $scope.standings = data;
+})
+
+// MODAL CONTROLLER
+.controller('ModalController', function($scope, $ionicModal) {
+  $ionicModal.fromTemplateUrl('my-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
 });
 
 });
